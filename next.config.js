5b98103a114af3177cdd6f40d6aeda918d154780
Sk Soyeb Akhter <sksoyebakhter2003@gmail.com>
2022-12-23 13:05:26 +0530

    Lint checking turned off

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com']
-  }
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
